50ff80afbe4e9fb6c4aa5abffc6ffd136d897a0a
LEA FRANCOIS <lea.jeane.francois@gmail.com>
2025-10-30 19:42:31 +0100

    initial deposit of bento component and graphic chart css

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,16 @@
+import BentoGrid from "../components/BentoGrid";
+import "./Home.css";
+
 function Home() {
-	return <h1>Home page</h1>;
+	return (
+		<>
+	<h1>Tatooine interim</h1>
+	<h2>Une mandale ou rien</h2>	
+	<h2>Prestations jsp</h2>
+	<BentoGrid />
+
+
+	</>);
 }
 
 export default Home;
