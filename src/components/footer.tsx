1f8e3d9f30bec7c0f0d8ef6f420d6790b108edf0
cecilepardo <pardo.cle@gmail.com>
2025-10-30 16:04:46 +0100

    footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,15 @@
+
+interface FooterProps {
+  companyName?: string;
+  year?: number;
+}
+
+const Footer = ({ companyName = "Tatooine Interim", year = 2026 } : FooterProps) => {
+  return (
+    <footer className = "footer">
+      {companyName} © {year} - Tous droits réservés.
+    </footer>
+  );
+};
+
+export default Footer;
