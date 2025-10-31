5c81c48f6ce8e59ff631a62166ed65968656bfc9
Pierre FOURDIN <fourdin.pfmg@gmail.com>
2025-10-31 13:32:39 +0100

    add secret page and css  and modify app,main

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import Accordeon from "./components/Accordeon";
 import { DataProvider } from "./components/DataProvider";
 import GeneralHeader from "./components/GeneralHeader";
@@ -6,17 +6,24 @@ import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
-	return (
-		<DataProvider>
-			<nav>
-				<GeneralHeader />
-			</nav>
-			<main>
-				<Outlet />
-			</main>
-			<Footer />
-		</DataProvider>
-	);
+  const location = useLocation();
+  const isSecretPage = location.pathname === "/";
+
+  return (
+    <DataProvider>
+      {!isSecretPage && (
+        <nav>
+          <GeneralHeader />
+        </nav>
+      )}
+
+      <main>
+        <Outlet />
+      </main>
+
+      <Footer />
+    </DataProvider>
+  );
 }
 
 export default App;
