1bee231dea21fc07c3efc93d5115a0f6f47e8952
cecilepardo <pardo.cle@gmail.com>
2025-10-31 09:43:40 +0100

    footer, card interface

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,30 @@
 import { useContext } from "react";
 import DataContext from "../contexts/DataContext";
 
-function Card = {
+interface dataCardPrezI {
+    titreh2: string;
+    imgSrc: string;
+    imgAlt: string;
+    titreh3: string;
+    paragraphe: string;
+    buttonLink: string;
+    buttonText: string;
+    reverse: boolean;
+}
+
+interface CardRecrutmentI {
+    dataCardPrez: dataCardPrezI;
+}
+
+
+function Card () {
   const {data} = useContext (DataContext);
   return (
     <div>
-    <img src={data.image} alt={data.name} />;
-    <h3> {data.name}</h3>
-    <p>{data.homeworld}</p>
-    <p>{data.species}</p>
+      <img src={data.image} alt={data.name} />;
+      <h3> {data.name}</h3>
+      <p>{data.homeworld}</p>
+      <p>{data.species}</p>
     {/* //button// 
     // demander plus d'informations pour le recrutement */}
     </div>
